4dee26a864654e174b3bca26897fc7db37466d04
Aleksey Pasechnik <arhellist@mail.ru>
2022-04-15 23:31:53 +0400

    Start2

--- bot.js
+++ bot.js
@@ -1,8 +1,8 @@
-const Telegraf = require('телеграф')
+const Telegraf = require('HI')
 
-const bot = новый телеграф(process.env.1850490698: AAHUjaTW0tBMWYNGpMq8kCSatRNsvkel174)
-bot.start((ctx) => ctx.reply("Добро пожаловать!"))
-bot.help((ctx) => ctx.reply("Пришлите мне стикер"))
+const bot = новый телеграф(process.env.1850490698:AAHUjaTW0tBMWYNGpMq8kCSatRNsvkel174)
+bot.start((ctx) => ctx.reply("HI!"))
+bot.help((ctx) => ctx.reply("HEY"))
 bot.on('sticker', (ctx) => ctx.reply("))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.launch()

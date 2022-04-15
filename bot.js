7f257359cf5acc4c08e8b728e9c41dc1b5dd3bfd
arhellist <92224498+arhellist@users.noreply.github.com>
2022-04-15 22:17:23 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,2 @@
 const Telegraf = require('telegraf')
 
-const bot = new Telegraf(process.env.1850490698: AAHUjaTW0tBMWYNGpMq8kCSatRNsvkel174)
-bot.start((ctx) => ctx.reply('Welcome!'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply(''))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
